ba9b06fc9621d064996f9214c913df5749979c61
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-01 17:36:18 -0400

    Calculator.jsx
    -Import Display
    -Remove extra spacing from state
    -Add displayValue to render function
    -Add Display component to render function

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,21 +1,17 @@
 import React, { Component } from 'react'
+import Display from '../Display/Display'
 import './Calculator.css'
 
 class Calculator extends Component {
   state = {
-
     // value to be displayed in the Display component
     displayValue: 0,
-
     // values to be displayed in the number keys component
     numbers: [],
-
     // values to be displayed in the operator keys
     operators: [],
-
     // selected Math operation
     selectedOperator: '',
-
     // stored value to use for the Math operation
     storedValue: ''
   }
@@ -33,8 +29,11 @@ class Calculator extends Component {
   }
 
   render = () => {
+    const { displayValue } = this.state
     return (
-      <div className='calculator-container'></div>
+      <div className='calculator-container'>
+        <Display displayValue={displayValue} />
+      </div>
     )
   }
 }
