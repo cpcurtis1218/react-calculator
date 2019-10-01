d442eeab271225facee565384d76e055e3782df5
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-01 16:54:18 -0400

    Calculator.jsx
    -Add Calculator component

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class Calculator extends Component {
+  state = {
+
+    // value to be displayed in the Display component
+    displayValue: 0,
+
+    // values to be displayed in the number keys component
+    numbers: [],
+
+    // values to be displayed in the operator keys
+    operators: [],
+
+    // selected Math operation
+    selectedOperator: '',
+
+    // stored value to use for the Math operation
+    storedValue: ''
+  }
+
+  callOperator = () => {
+    console.log('call operation')
+  }
+
+  setOperator = () => {
+    console.log('set operation')
+  }
+
+  updateDisplay = () => {
+    console.log('update display')
+  }
+
+  render = () => {
+    return (
+      <div className='calculator-container'></div>
+    )
+  }
+}
+
+export default Calculator
