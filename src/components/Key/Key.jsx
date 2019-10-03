a254ba54eb28304121555b528e9febf419318963
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-10-03 16:47:49 -0400

    Add Key component to Key.js

---
+++ src/components/Key/Key.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Key.css'
+
+const Key = ({ keyAction, keyType, keyValue }) => {
+  return (
+    <div className='key-container'>
+      <p className='key-value'>
+        {keyValue}
+      </p>
+    </div>
+  )
+}
+
+Key.propTypes = {
+  keyAction: PropTypes.func.isRequired,
+  keyType: PropTypes.string.isRequired,
+  keyValue: PropTypes.string.isRequired
+}
+
+export default Key
